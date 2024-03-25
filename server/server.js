52703e3626e099dec7213743acaee0f234b69352
Nessim Yohros <nessim@MacBook-Pro-109.local>
2024-03-24 21:15:15 -0500

    Fix Heroku PORT binding issue

--- server/server.js
+++ server/server.js
@@ -44,6 +44,7 @@ app.get('/translate', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
