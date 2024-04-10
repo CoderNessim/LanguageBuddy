e5f7811c74091cbed8ee98b152b25eb54a731d40
Nessim Yohros <nessim@MacBook-Pro-109.local>
2024-04-10 17:10:21 -0300

    fixed an issue with react routing

--- client/src/components/FlashcardComponents/ViewAllHeader.jsx
+++ client/src/components/FlashcardComponents/ViewAllHeader.jsx
@@ -1,18 +1,18 @@
-import ModalHeader from "../ModalWindow/ModalHeader"
-import { CloseButton } from "@mantine/core"
-import { useNavigate } from "react-router-dom"
+import ModalHeader from '../ModalWindow/ModalHeader';
+import { CloseButton } from '@mantine/core';
+import { useNavigate } from 'react-router-dom';
 function ViewAllHeader() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <div className="all-flashcards-header">
-        <ModalHeader title="All Flashcards" />
-        <CloseButton
-          onClick={() => navigate(-1)}
-          style={{ alignSelf: 'center' }}
-          size="xl"
-        />
-      </div>
-  )
+      <ModalHeader title="All Flashcards" />
+      <CloseButton
+        onClick={() => navigate('/')}
+        style={{ alignSelf: 'center' }}
+        size="xl"
+      />
+    </div>
+  );
 }
 
-export default ViewAllHeader
+export default ViewAllHeader;
